bc9cad5f1d069206d810abbbf5d24ad2383a45e2
bulatovad <damira.bulatova@gmail.com>
2024-02-04 00:45:30 +0600

    updated ChainParserBuilder

--- task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
@@ -7,15 +7,17 @@ public class ChainParserBuilder {
     private List<AbstractTextParser> parsers = new ArrayList<>();
 
     public ChainParserBuilder() {
+
     }
 
     public ChainParserBuilder setParser(AbstractTextParser abstractTextParser) {
         // Write your code here!
+        parsers.add(abstractTextParser);
         return this;
     }
 
     public AbstractTextParser build() {
         // Write your code here!
-        return null;
+        return parsers.get(0).nextParser;
     }
 }
